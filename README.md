acbf1af637060d5ff12611dd1465840cb17becd2
James-Livesey <jamesliveg@gmail.com>
2019-05-06 17:08:57 +0100

    Add base repo structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Zapr
 Zapr; the app-building system for LiveG.
+
+This repository is licensed by the [LiveG Open-Source Licence](https://github.com/LiveG-Development/Zapr/blob/master/LICENCE.md).
---
+++ zapr.py
@@ -0,0 +1,10 @@
+# Zapr
+# 
+# Copyright (C) LiveG. All Rights Reserved.
+# Copying is not a victimless crime. Anyone caught copying LiveG software may
+# face sanctions.
+# 
+# https://liveg.tech
+# Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
+
+print("Hello, world!")
